6087ed1b2128b5e2ae7f681dcd5b722e23ab95cf
Maor Kern <maor.kern@gmail.com>
2015-08-28 14:26:20 +0300

    goddamnit

---
+++ application/controllers/Ping.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ping extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        echo "pong </br> groupic version 0.1 </br> Made solely by Maor";
+    }
+
+
+}
