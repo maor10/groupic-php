2ec32315e405d02ce284f19e2992fca4fd3c84fd
Maor Kern <maor.kern@gmail.com>
2015-08-29 11:45:53 +0300

    ss

--- application/models/Photos_model.php
+++ application/models/Photos_model.php
@@ -15,7 +15,7 @@ class Photos_model extends CI_Model {
      * @return : an array of objects each representing a picture ('Picture' field represents filename)
      */
     public function getEventPictures($eventID) {
-        $this->db->select("*")->from("events_photos")->where("event_id", $eventID);
+        $this->db->select("*")->from("events_photos")->where("event_id", $eventID)->order_by("timestamp", "desc"); ;
         $result = $this->db->get()->result();
         return $result;
     }
